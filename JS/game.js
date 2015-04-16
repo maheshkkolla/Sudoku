60e6a3c4173901f2c0765a442b688f9d552b2ebc
Mahesh <mahesh@INMaheshKumar.local>
2015-04-16 19:20:59 +0530

    created json and storing game board in a specific json format

--- JS/game.js
+++ JS/game.js
@@ -1,86 +1,94 @@
-var game = [];
-var firstSubBoard = [[],[],[]];
+var fs = require('fs');
+var board = JSON.parse(fs.readFileSync("./Data/board.json"));
 
 var generateNewGame = function() {
+	initializeBoard();
 	fillBoard();
-	showNumbersOnBoard();
+	fs.writeFileSync("./Data/board.json", JSON.stringify(board));
+	showBoard();
+}
 
+var showBoard = function() {
+	var keys = Object.keys(board);
+	keys.forEach(function(key){
+		$("#"+key).html(board[key]["value"]);
+	});
+}
+
+var getKeyFrom = function(row,col){
+	return row.toString() + col.toString();
 }
 
 var fillBoard = function() {
-	fillSubBoardOne();
+	var subBoard = getSubBoard();
+	var tempBoard = [];
 	for(var i=0;i<3;i++){
-		game.push(getRowOne());
-		game.push(getRowTwo());
-		game.push(getRowThree());
-		changeSubBoard();
+		tempBoard.push(getRowOne(subBoard));
+		tempBoard.push(getRowTwo(subBoard));
+		tempBoard.push(getRowThree(subBoard));
+		subBoard = getChangedSubBoard(subBoard);
 	}
-	swapCols();	
+	fillBoardWith(tempBoard);
 }
 
-var swapCols = function() {
-	for(var col=0; col<9; col+=3){
-		var index1 = col + getRandomNumberBelow(3);
-		var index2 = col + getRandomNumberBelow(3);
-		for(var row=0; row<9; row++){
-			var temp = game[row][index1];
-			game[row][index1] = game[row][index2];
-			game[row][index2] = temp;
+var getChangedSubBoard = function(subBoard) {
+	var newSubBoard = [[],[],[]];
+	for(var row=0;row<3;row++){
+		newSubBoard[row].push(subBoard[row][2]);
+		newSubBoard[row].push(subBoard[row][0]);
+		newSubBoard[row].push(subBoard[row][1]);
+	}
+	return newSubBoard;
+}
+
+var getRowOne = function(subBoard) {
+	return(subBoard[0].concat(subBoard[2]).concat(subBoard[1]));
+}
+var getRowTwo = function(subBoard) {
+	return(subBoard[1].concat(subBoard[0]).concat(subBoard[2]));
+}
+var getRowThree = function(subBoard) {
+	return(subBoard[2].concat(subBoard[1]).concat(subBoard[0]));
+}
+
+var fillBoardWith = function(tempBoard){
+	for(var row=0;row<tempBoard.length;row++){
+		for(var col=0; col<tempBoard.length;col++){
+			var key = getKeyFrom(row+1,col+1);
+			board[key]['value'] = tempBoard[row][col];
 		}
 	}
 }
 
-// var swapRows = function() {
-// 	for(var i=0;i<9;i+=3){
-// 		var index1 = i + getRandomNumberBelow(3);
-// 		var index2 = i + getRandomNumberBelow(3);
-// 		var temp = game[index1];
-// 		game[index1] = game[index2];
-// 		game[index2] = temp;
-// 	}
-// }
 
-var changeSubBoard = function() {
-	var newSubBoard = [[],[],[]];
-	for(var row=0;row<3;row++){
-		newSubBoard[row].push(firstSubBoard[row][2]);
-		newSubBoard[row].push(firstSubBoard[row][0]);
-		newSubBoard[row].push(firstSubBoard[row][1]);
-	}
-	firstSubBoard = newSubBoard;
-}
-
-var fillSubBoardOne = function() {
+var getSubBoard = function() {
+	var subBoard = [];
 	var numbers = [1,2,3,4,5,6,7,8,9];
 	for(var row=0;row<3;row++){
+		subBoard.push([]);
 		for(var col=0;col<3;col++){
 			var index = getRandomNumberBelow(numbers.length);
-			firstSubBoard[row].push(numbers[index]);
+			subBoard[row].push(numbers[index]);
 			numbers.splice(index,1);
 		}
 	}
-}
-
-var getRowOne = function() {
-	return(firstSubBoard[0].concat(firstSubBoard[2]).concat(firstSubBoard[1]));
-}
-var getRowTwo = function() {
-	return(firstSubBoard[1].concat(firstSubBoard[0]).concat(firstSubBoard[2]));
-}
-var getRowThree = function() {
-	return(firstSubBoard[2].concat(firstSubBoard[1]).concat(firstSubBoard[0]));
+	return subBoard;
 }
 
 var getRandomNumberBelow = function(number) {
 	return Math.floor(Math.random() * number);
 }
 
-
-var showNumbersOnBoard = function() {
-	game.forEach(function(row, rowNo){
-		row.forEach(function(col, colNo){
-			var id = (rowNo + 1).toString() + (colNo + 1).toString();
-			$("#"+id).html(col);
-		});
-	}); 
+var initializeBoard = function() {
+	var subId = 1;
+	for(var row=1;row<=9;row++) {
+		for(var col=1;col<=9;col++) {
+			var key = row.toString() + col.toString();
+			board[key] = {"value":"\t", "readOnly":false, "subId": subId,"ans":"\t"};
+			if(col % 3 == 0) subId++;
+		}
+		if((subId-1) % 3 == 0 && row % 3 != 0){
+			subId = subId - 3;
+		}
+	}
 }
